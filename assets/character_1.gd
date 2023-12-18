b554e80fb48d7f33812466b37ebf3443fb168989
Yuxuan Liu <97258813+Yu1623@users.noreply.github.com>
2023-12-17 20:36:42 -0800

    Dialogue Interaction
    Set up the basics of the dialogue system. Probably need more improvements.

---
+++ assets/character_1.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var moveSpeed: float = 100
+
+@onready var actionableFinder: Area2D = $Direction/ActionableFinder
+
+func _physics_process(_delta):
+	var inputDirection = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	velocity = inputDirection * moveSpeed
+	
+	move_and_slide()
+
+func _unhandled_input(_event) -> void:
+	if Input.is_action_just_pressed("interactDialogue"):
+		var actionables = actionableFinder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return

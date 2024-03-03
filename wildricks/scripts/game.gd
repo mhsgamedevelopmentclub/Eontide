7571f88c520f0b08921e3adb9772a2f85382d288
Yuxuan Liu <liu.family.yx@gmail.com>
2024-03-02 16:33:19 -0800

    Dialogue Edit
    Branching dialogue with more than one NPC.

--- wildricks/scripts/game.gd
+++ wildricks/scripts/game.gd
@@ -2,8 +2,6 @@ class_name GameManager
 extends Node2D
 
 @onready var battle_scene := preload('res://scenes/battle_scene.tscn')
-@onready var enemy_scene := preload('res://assets/enemy_1.tscn')
-@onready var battle_scene := preload('res://scenes/battle.tscn')
 @onready var enemy_scene := preload('res://scenes/enemy_1.tscn')
 @onready var pause_menu := $"Pause Menu"
 

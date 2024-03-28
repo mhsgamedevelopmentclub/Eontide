a88b2af51641cbbc9f4448b4e0c85d6adc01d9a3
Yuxuan Liu <liu.family.yx@gmail.com>
2024-03-27 18:17:00 -0700

    Dialogue 1

--- wildricks/scripts/scene_transition.gd
+++ wildricks/scripts/scene_transition.gd
@@ -1,6 +1,11 @@
 extends CanvasLayer
 
-@onready var anim_player := $AnimationPlayer
+signal transitioned
+# Called when the node enters the scene tree for the first time.
 
 func transition():
-	anim_player.play("fade_transition")
+	$AnimationPlayer.play("fade_transition")
+	
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "fade_transition":
+		emit_signal("transitioned")
--- wildricks/scripts/town_scene.gd
+++ wildricks/scripts/town_scene.gd
@@ -5,22 +5,27 @@ extends Node2D
 <<<<<<< HEAD
 var square_scene: String = "res://scenes/square_scene.tscn"
 # Called when the node enters the scene tree for the first time.
-=======
 var scene_animation_complete: bool = false
-
->>>>>>> 735f46b5ebddd870ad8ccc11fee7017bd6b75946
-func _ready():
-	$Scene_Transition.anim_player.connect('animation_finished', change_scene)
+#func _ready():
+#	$Scene_Transition.anim_player.connect('animation_finished', change_scene)
 
 func _process(_delta):
 	if gameState.go_to_square == true:
-<<<<<<< HEAD
 		get_tree().change_scene_to_file(square_scene)
+		#$Scene_Transition.transition()
 
-		
+#func change_scene(_anim_name) -> void:
+#	get_tree().change_scene_to_file("res://scenes/square_scene.tscn")
 =======
-		$Scene_Transition.transition()
+var scene_animation_complete: bool = false
+
+func _ready():
+    $Scene_Transition.anim_player.connect('animation_finished', change_scene)
+
+func _process(_delta):
+    if gameState.go_to_square == true:
+        $Scene_Transition.transition()
 
 func change_scene(_anim_name) -> void:
-	get_tree().change_scene_to_file("res://scenes/square_scene.tscn")
->>>>>>> 735f46b5ebddd870ad8ccc11fee7017bd6b75946
+    get_tree().change_scene_to_file("res://scenes/square_scene.tscn")
+>>>>>>> 0d97e6f34882098b8a26393418c5a92ebe5a0c53

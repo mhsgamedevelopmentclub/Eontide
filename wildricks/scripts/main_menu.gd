b172a69f6e78ea39fefbc1f2a08e69177ce1b7e4
Yuxuan Liu <liu.family.yx@gmail.com>
2024-05-04 16:10:12 -0700

    Achievements Complete

--- wildricks/scripts/main_menu.gd
+++ wildricks/scripts/main_menu.gd
@@ -5,12 +5,14 @@ extends Control
 @onready var load_game_button := $"HBoxContainer/VBoxContainer/Load Game"
 @onready var settings_button := $HBoxContainer/VBoxContainer/Settings
 @onready var credits_button := $HBoxContainer/VBoxContainer/Credits
+@onready var achievements_button: Button = $"HBoxContainer/VBoxContainer/Achievements"
 
 func _ready() -> void:
 	new_game_button.connect('pressed', new_game)
 	load_game_button.connect('pressed', load_game)
 	settings_button.connect('pressed', open_settings)
 	credits_button.connect('pressed', open_settings)
+	achievements_button.connect('pressed', open_achievement)
 
 func new_game() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
@@ -23,3 +25,6 @@ func open_settings() -> void:
 
 func open_credits() -> void:
 	pass
+
+func open_achievement() -> void:
+	get_tree().change_scene_to_file("res://achievements_menu.tscn")
